b175ab36a75740de47e9236295db38b97f1a511f
grant <grant.enderkids@gmail.com>
2024-07-23 09:26:31 -0600

    scanned the files for bugs to see what was preventing the min input to start at zero, I was able to pinpoint to the problem

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const x = '23';
 
 const calcAge = birthYear => 2024 - birthYear;
 console.log(calcAge(1987));
-*/
+
 
 //We work for a company building a smart home therometer. Our most recent task is this: "Given an array of temeratures of one day, calculate the termerature amplitude. Keep in mind that sometimes there might be a senso error"
 
@@ -66,3 +66,48 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 4, 1], [9, 0, 1, 5]);
 console.log(amplitudeNew);
+
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) Fix
+    // value: Number(prompt('Degrees Celsius:')),
+    value: 10,
+  };
+  // B) Find
+  console.log(measurement);
+  console.table(measurement);
+  10;
+  // console.log(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) Identify
+console.log(measureKelvin());
+
+// Using a De-Bugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const array1 = ['a', 'b', 'c'];
+  const array2 = ['d', 'e', 'f'];
+  const array3 = array1.concat(array2);
+
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) Identify:
+console.log(amplitudeBug);
